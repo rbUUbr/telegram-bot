2485abad89300c5270ef545c4a3e97f2eaa9ffc0
rbuDeveloper <kirabsuir@gmail.com>
2016-10-31 21:56:29 +0300

    First level complete

--- lib/Subjects.rb
+++ lib/Subjects.rb
@@ -5,27 +5,57 @@ class SubjectsCommand < MainCommand
       super(message, bot, text)
       user_action = 'subject'
     end
+    def make_array_of_subjects(array_of_lines)
+      result = []
+      array_of_lines.each do |line|
+        result.push(line.delete!("Subject:")) if line.include?("Subject:")
+      end
+      result
+    end
+    def is_subject_in_hash?(array_of_lines, subject)
+      result = false
+      array_of_lines.each do |line|
+        result = true if line.include?("#{subject}")
+      end
+      result
+    end
+    def find_subject_in_file_or_not?(array_of_lines, subject)
+      array_of_lines = SubjectsCommand.delete_special_chars(array_of_lines)
+      array_of_lines = SubjectsCommand.make_array_of_subjects(array_of_lines)
+      result = SubjectsCommand.is_subject_in_hash?(array_of_lines, subject)
+      return result
+    end
     def open_and_check(id, subject)
       new_subject = true
-      array_of_lines = IO.readlines("./users/#{id}")
-      array_of_lines = SubjectsCommand.delete_special_chars(array_of_lines)
-      new_subject = false if array_of_lines.include?(subject)
+      if File.exist?("./users/#{id}")
+        array_of_lines = IO.readlines("./users/#{id}")
+        new_subject = SubjectsCommand.find_subject_in_file_or_not?(array_of_lines, subject)
+      end
       new_subject
     end
-    def end_command(bot, message, id, subject, count_of_labs, user_subjects)
-      case open_and_check(id, subject)
-      when true
-        write_subject_to_file(id, subject, count_of_labs, user_subjects)
-        bot.api.send_message(chat_id: message.chat.id, text: "Давай сдавай")
-      when false
-        bot.api.send_message(chat_id: message.chat.id, text: "Такие дела. У тебя уже есть этот предмет в списке сдаваемых")
+    def check_correct?(text)
+      if text.to_i <= 0 || text.to_i > 40
+        return false
+      end
+      return true
+    end
+    def end_command(bot, message, id, subject, count_of_labs, user_subjects, user_action)
+      if check_correct?(message.text)
+        case open_and_check(id, subject)
+        when false
+          write_subject_to_file(id, subject, count_of_labs, user_subjects)
+          bot.api.send_message(chat_id: message.chat.id, text: "Давай сдавай")
+        when true
+          bot.api.send_message(chat_id: message.chat.id, text: "У тебя уже есть этот предмет в списке сдаваемых")
+        end
+      else
+        bot.api.send_message(chat_id: message.chat.id, text: "Пссс. количество лаб надо вводить числом(или цифрой). Давай по новой")
+        user_action = 'count_of_labs'
       end
     end
     def write_strings_for_new_subjects(id_file, subject, count_of_labs, user_subjects)
-      id_file.write("Subject:\n")
-      id_file.write("#{subject}\n")
-      id_file.write("Count of labs:\n")
-      id_file.write("#{count_of_labs}\n")
+      id_file.write("Subject:#{subject}\n")
+      id_file.write("Count of labs:#{count_of_labs}\n")
       user_subjects[subject.to_sym] = count_of_labs
     end
     def write_subject_to_file(id, subject,count_of_labs, user_subjects)
--- semester-tg-bot.rb
+++ semester-tg-bot.rb
@@ -12,7 +12,6 @@ require_relative './lib/Subjects.rb'
 require_relative './lib/Reset.rb'
 require_relative './lib/calculations.rb'
 TOKEN = '298272856:AAEMgCksHqhx__47RvnrgWkk7dq0Ycjt-Sw'
-user = User.new
 $new_client = false
 temp = []
 def intro(user_name)
@@ -22,6 +21,8 @@ $redis = Redis.new
 temp = ''
 command = MainCommand.new
 Telegram::Bot::Client.run(TOKEN) do |bot|
+  user = User.new
+  user.subjects = {}
   bot.listen do |message|
     command.name_of_the_command = message.text
     case command.name_of_the_command
@@ -29,7 +30,6 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
       MainCommand.open_and_check(message.from.id)
       user.name = message.from.first_name
       MainCommand.command(message, bot, intro(user.name))
-      $redis.set("kek", user.name)
     when '/semester'
     	user.action = 'semester'
     	SemesterCommand.command(message, bot, "Так-с, что это тут у нас? Новый семестр? Давай обозначим дедлайн. Для этого скажи мне, с какого ты курса:", user.action)
@@ -37,23 +37,28 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
       user.action = 'subject'
       SubjectsCommand.command(message, bot, "Что сдаём?", user.action)
     when '/status'
-     StatusCommand.command(message, bot,"Так-с, что тебе надо сдать...Ах, да, вот что:", user.subjects)
+      user.subjects = {}
+      user.load_object(message.from.id)
+      StatusCommand.command(message, bot,"Так-с, что тебе надо сдать...Ах, да, вот что:", user, user.end_semester, user.subjects, message.from.id)
     when '/reset'
-      ResetCommand.command(user.subjects, user.start_semester, user.end_semester)
+      ResetCommand.command(user.subjects, user.start_semester, user.end_semester, message.from.id)
+      bot.api.send_message(chat_id: message.chat.id, text: "Только тссссс. Не надо всем говорить, что сдал лабы, а на самом деле просто отменил их в своём боте")
     when '/submit', /я сдал/
       MainCommand.command(message, bot, 'красава')
+    when ""
+      MainCommand.command(message, bot, intro(user.name))
     else
     	case user.action
     	when 'semester'
-        SemesterCommand.semester(message, bot, user, message.from.id)
-        #write_semester_to_file(message.from.id, user.start_semester, user.end_semester)
+        SemesterCommand.semester(message, bot, user, message.from.id, user.action)
+      #  user.write_semester_to_file(message.from.id, user.start_semester, user.end_semester)
       when 'subject'
         bot.api.send_message(chat_id: message.chat.id, text: "Сколько лаб по предмету?")
         temp = message.text
         user.action = 'count_of_labs'
       when 'count_of_labs'
-        SubjectsCommand.end_command(bot, message, message.from.id, temp, message.text, user.subjects)
-        bot.api.send_message(chat_id: message.chat.id, text: "Оке")
+        SubjectsCommand.end_command(bot, message, message.from.id, temp, message.text, user.subjects, user.action)
+
       end
     end
   end
